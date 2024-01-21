49773e9423c7020b3178ce7dada59aa5a6039182
Dainis Verdins <55252915+DainisVerdins@users.noreply.github.com>
2024-01-21 20:18:05 +0200

    init repo

---
+++ RestTestingField/RestClientService/CORS/Queries/GetWeatherForecastQuery.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+
+namespace RestClientService.CORS.Queries;
+
+public class GetWeatherForecastQuery : IRequest<IEnumerable<WeatherForecast>>
+{
+}
+
+public class GetWeatherForecastQueryHandler : IRequestHandler<GetWeatherForecastQuery, IEnumerable<WeatherForecast>>
+{
+    private static readonly string[] Summaries = new[]
+           {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    public async  Task<IEnumerable<WeatherForecast>> Handle(GetWeatherForecastQuery request, CancellationToken cancellationToken)
+    {
+        return await Task.Run(() => {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            }).AsEnumerable();
+        });
+    }
+}
---
+++ RestTestingField/RestClientService/Controllers/WeatherForecastController.cs
@@ -0,0 +1,33 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using RestClientService.CORS.Queries;
+
+namespace RestClientService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IMediator _mediator;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public  async Task<IActionResult> Get()
+        {
+            var result = await  _mediator.Send(new GetWeatherForecastQuery());
+
+            return Ok(result);
+        }
+    }
+}
